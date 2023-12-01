83f90e65d024f9f8c93498950e9dad354b015837
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-12-01 10:36:40 +0100

    create lib for verifying tokens

---
+++ src/lib/verifyToken.ts
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+export const verify = (req: Express.Request & { headers: { token: string }, user: any }, res: Express.Response, next) => {
+    const authHeader = req.headers?.token
+
+    if (authHeader) {
+        const token = authHeader.split(" ")[1]; //the token in the header will be of the form "Bearer sfsdgsdfsdfsfs", so the actual token will be the second entry of the array when spitted
+
+        jwt.verify(
+            token, 
+            process.env.SECRET_KEY,
+            (err, data) => {
+                if (err) return res.status(403).json({msg: "Token is not valid"})
+
+                req.user = data;
+
+                next();
+            }
+        )
+    }
+    else {
+        return res.status(401).json({msg: "You are not authenticated"}); 
+    }
+}
