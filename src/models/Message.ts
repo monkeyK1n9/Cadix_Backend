776ca3deec0a648f747afa01c11fd4820c4d9952
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-12-02 11:46:15 +0100

    create message schema

---
+++ src/models/Message.ts
@@ -0,0 +1,9 @@
+import { randomUUID } from "crypto";
+import mongoose from "mongoose";
+
+// handle the message groups
+const messageGroupSchema = new mongoose.Schema({
+    
+}, {
+    timestamps: true
+})
