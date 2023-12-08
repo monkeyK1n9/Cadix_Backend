7557abced6eda686d15baab0d893b161a6bbee1d
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-12-08 15:35:00 +0100

    implement verification of OTP;

--- src/middlewares/verifyotp.ts
+++ src/middlewares/verifyotp.ts
@@ -1,26 +1,87 @@
 import jwt from 'jsonwebtoken';
+import { UserOTPVerification } from '../models/UserOTPVerification';
 const User = require('../models/User');
 
+
 export async function verifyOTP(req: any, res: any) {
-    const {data} = req.body;
-    // we fetch the stored user in database
-    const storedUser = User.findOne({
-        _id: data.userId
-    })
+    try {
 
-    // create an access token and sending back to the client
-    const accessToken = jwt.sign(
-        {
-            id: data.userId
-        },
-        process.env.SECRET_KEY as string, //sending the decrypting secret phrase
-        {
-            expiresIn: "5d"
+        const {userId, otp} = req.body;
+        if (!userId || !otp) {
+            throw new Error("Empty otp defails are not allowed")
         }
-    )
 
-    // removing password from the data we send back to the client
-    const { password, ...userInfo } = storedUser._doc;
+        const userOTPVerificationRecords = await UserOTPVerification.find({
+            userId
+        })
 
-    return res.status(200).json({  accessToken, ...userInfo });
+        // check if the user has otp recodrd in database
+        if (userOTPVerificationRecords.length <= 0) {
+            // we tell the user he has no otp record and needs to sign up
+            throw new Error("Account record does not exist or has been verified already. Please sign up or log in")
+        }
+        else {
+            // user otp record exists
+            const userOTPRecord = userOTPVerificationRecords[0];
+            const encryptedOTP = userOTPVerificationRecords[0].otp;
+
+            // check if the otp is still valid after the time passed ( we allowed 60 mins)
+            if (userOTPRecord.expiredAt.getTime() < Date.now()){
+                // the otp has expired, we delete all user's records
+                await UserOTPVerification.deleteMany({
+                    userId
+                })
+                // we throw error to alert about code expiration
+                throw new Error("Code has expired. Please request again.")
+            }
+            else {
+                // if time validity is correct, we compare the receive OTP and the one in the database
+                const decryptedOTP = CryptoJS.AES.decrypt(encryptedOTP, process.env.SECRET_KEY as string);
+
+                if (otp != decryptedOTP) {
+                    // supplied code is invalid
+                    throw new Error("Invalid code passed. Check your inbox.");
+                }
+                else {
+                    // correct otp, we update verification status of user
+                    await User.updateOne(
+                        { _id: userId },
+                        { isVerified: true },
+                    );
+
+                    // we deleted the user's records
+                    await UserOTPVerification.deleteMany({
+                        userId
+                    })
+
+                    // we fetch the stored user in database
+                    const storedUser = User.findOne({
+                        _id: userId
+                    })
+                
+                    // create an access token and sending back to the client
+                    const accessToken = jwt.sign(
+                        {
+                            id: userId
+                        },
+                        process.env.SECRET_KEY as string, //sending the decrypting secret phrase
+                        {
+                            expiresIn: "5d"
+                        }
+                    )
+                
+                    // removing password from the data we send back to the client
+                    const { password, ...userInfo } = storedUser._doc;
+                
+                    return res.status(200).json({  accessToken, ...userInfo });
+                }
+            }
+        }
+    }
+    catch (err: any) {
+        return res.json({
+            status: "FAILED",
+            message: err.message
+        })
+    }
 }
