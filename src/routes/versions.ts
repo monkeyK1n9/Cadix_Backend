7905b7fd5318e9a9d4497b8116b08e69626db647
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-12-10 18:44:29 +0100

    create versions route

--- src/routes/versions.ts
+++ src/routes/versions.ts
@@ -1,3 +1,28 @@
 import router from 'express';
+import { verify } from '../lib/verifyToken';
+import { 
+    createVersion, 
+    deleteVersion, 
+    getAllVersions, 
+    getVersion, 
+    updateVersion
+} from '../middlewares/versions';
 
 const versionsRouter = router.Router();
+
+// CREATE VERSION
+versionsRouter.post("/", verify, createVersion);
+
+// DELETE VERSION
+versionsRouter.delete("/", verify, deleteVersion);
+
+// UPDATE VERSION
+versionsRouter.put("/", verify, updateVersion);
+
+// GET VERSIONS
+versionsRouter.get("/", verify, getAllVersions);
+
+// GET VERSION
+versionsRouter.get("/", verify, getVersion);
+
+export { versionsRouter };
