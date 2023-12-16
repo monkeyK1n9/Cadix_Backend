98778ca98da687942488fe97bf51c06f4d2f3fe6
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-12-16 11:28:35 +0100

    implement create team

--- src/middlewares/teams.ts
+++ src/middlewares/teams.ts
@@ -1,11 +1,52 @@
+import { Project, ProjectTeam } from "../models/Project";
 
 
-export async function createTeam() {
+export async function createTeam(req: any, res: any) {
     try {
-        
+        const { userId, projectId, groupAdmins, teamMembers } = req.body;
+
+        // user can only create a team if he is a project admin or project owner
+        const project = await Project.findOne(
+            {
+                _id: projectId
+            }
+        )
+
+        if(!project) {
+            throw new Error("Project not found")
+        }
+        else {
+            if(!project.projectAdmins.includes(userId) || project.createdBy != userId) {
+                throw new Error("User is not authorized to create a team");
+            }
+            else {
+                // we create the team
+                const newProjectTeam = new ProjectTeam({
+                    projectId,
+                    groupAdmins,
+                    teamMembers
+                })
+
+                const projectTeam = await newProjectTeam.save();
+
+                //add the team to the project
+                await Project.findOneAndUpdate(
+                    {
+                        _id: projectId
+                    },
+                    {
+                        $push: {
+                            teams: projectTeam._id
+                        }
+                    }
+                )
+
+                return res.status(200).json(projectTeam);
+            }
+        }
     }
     catch(err: any) {
-
+        return res.json({ message: "Failed to create team: " + err });
     }
 }
 
