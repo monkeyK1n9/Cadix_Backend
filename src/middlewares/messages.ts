2b292cd225dbf7062e4cba831dfade6ad2fba78f
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-12-19 11:58:12 +0100

    create messages middlewares

---
+++ src/middlewares/messages.ts
@@ -0,0 +1,46 @@
+
+
+export async function createMessage(req: any, res: any) {
+    try {
+
+    }
+    catch (err: any) {
+
+    }
+}
+
+export async function deleteMessage(req: any, res: any) {
+    try {
+
+    }
+    catch (err: any) {
+
+    }
+}
+
+export async function updateMessage(req: any, res: any) {
+    try {
+
+    }
+    catch (err: any) {
+
+    }
+}
+
+export async function getAllMessages(req: any, res: any) {
+    try {
+
+    }
+    catch (err: any) {
+
+    }
+}
+
+export async function getMessage(req: any, res: any) {
+    try {
+        
+    }
+    catch (err: any) {
+    
+    }
+}
