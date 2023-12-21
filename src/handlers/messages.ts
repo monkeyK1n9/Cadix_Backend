ee893d3a89ec6b86ba9275973ff0c3734f40fee1
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-12-21 14:28:49 +0100

    create message handlers

--- src/handlers/messages.ts
+++ src/handlers/messages.ts
@@ -1,1 +1,13 @@
 // we create handlers for socker io
+
+export async function sendMessage() {
+
+}
+
+export async function receiveMessage() {
+
+}
+
+export async function sendNotification() {
+    
+}
